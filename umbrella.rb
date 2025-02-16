711502f6e1561769a514b608af12ae5d2bce3f26
Drew Krehel <drewkrehel@gmail.com>
2025-02-16 19:39:20 +0000

    geocode working

--- umbrella.rb
+++ umbrella.rb
@@ -18,3 +18,20 @@ currently_hash = parsed_response.fetch("currently")
 current_temp = currently_hash.fetch("temperature")
 
 puts "The current temperature is " + current_temp.to_s + "."
+
+puts "where are you located?"
+user_location = gets.chomp 
+# user_location = "Chicago"
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+resp = HTTP.get(maps_url)
+raw_response2 = resp.to_s
+parsed_response = JSON.parse(raw_response2)
+results = parsed_response.fetch("results")
+first_result = results.at(0)
+geo = first_result.fetch("geometry")
+loc = geo.fetch("location")
+pp latitude = loc.fetch("lat")
+pp longitutde = loc.fetch("lng")
