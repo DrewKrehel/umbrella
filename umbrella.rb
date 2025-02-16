fb9769a46eaf258b81a2ba0b965f1676f0931492
Drew Krehel <drewkrehel@gmail.com>
2025-02-16 21:42:04 +0000

    try 1

--- umbrella.rb
+++ umbrella.rb
@@ -2,23 +2,6 @@ require "http"
 require "json"
 require "dotenv/load"
 
-# Hidden variables
-pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
-
-# Assemble the full URL string by adding the first part, the API token, and the last part together
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/41.8887,-87.6355"
-
-# Place a GET request to the URL
-raw_response = HTTP.get(pirate_weather_url)
-
-parsed_response = JSON.parse(raw_response)
-
-currently_hash = parsed_response.fetch("currently")
-
-current_temp = currently_hash.fetch("temperature")
-
-puts "The current temperature is " + current_temp.to_s + "."
-
 puts "where are you located?"
 user_location = gets.chomp 
 # user_location = "Chicago"
@@ -35,3 +18,17 @@ geo = first_result.fetch("geometry")
 loc = geo.fetch("location")
 pp latitude = loc.fetch("lat")
 pp longitutde = loc.fetch("lng")
+
+# Hidden variables
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/#{latitude},#{longitutde}"
+
+# Place a GET request to the URL
+raw_response = HTTP.get(pirate_weather_url)
+parsed_response = JSON.parse(raw_response)
+
+currently_hash = parsed_response.fetch("currently")
+current_temp = currently_hash.fetch("temperature")
+puts "The current temperature is " + current_temp.to_s + "."
