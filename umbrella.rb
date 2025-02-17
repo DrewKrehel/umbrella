4a61badb4f456c185cf7f77b9b082800382565e1
Drew Krehel <drewkrehel@gmail.com>
2025-02-17 17:36:38 +0000

    improvements?

--- umbrella.rb
+++ umbrella.rb
@@ -3,12 +3,18 @@ require "json"
 require "dotenv/load"
 require "ascii_charts"
 
-puts "where are you located?"
+line_length = 40
+puts "=" * line_length
+puts "Will you need an umbrella today?".center(line_width)
+puts "=" * line_length
+puts
+puts "Where are you located?"
 # user_location = gets.chomp 
 # user_location = "Chicago"
 # user_location = "Philidelphia"
 user_location = "Seattle"
-pp user_location
+puts user_location
+puts "Checking the weather at #{user_location}...."
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
@@ -19,14 +25,16 @@ results = parsed_response.fetch("results")
 first_result = results.at(0)
 geo = first_result.fetch("geometry")
 loc = geo.fetch("location")
-pp latitude = loc.fetch("lat")
-pp longitutde = loc.fetch("lng")
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
+
+puts "Your coordinate are #{latitude}, #{longitude}"
 
 # Hidden variables
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 # Assemble the full URL string by adding the first part, the API token, and the last part together
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/#{latitude},#{longitutde}"
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/#{latitude},#{longitude}"
 
 # Place a GET request to the URL
 raw_response = HTTP.get(pirate_weather_url)
@@ -38,6 +46,12 @@ puts "The current temperature is " + current_temp.to_s + "."
 
 next_hour = currently_hash.fetch("summary")
 puts "Next hour: " + next_hour
+
+hourly_hash = parsed_response.fetch("hourly")
+hour_data = hourly_hash.fetch("data")
+next_12_hours = hour_data[0..11]
+# hour_precip = hour_data[0].fetch("precipProbability")
+puts "In 0 hours, there is a #{hour_data[0].fetch("precipProbability")*100}% chance of precipitation."
 
 forcast = currently_hash.fetch("precipType")
 case forcast
@@ -52,10 +66,7 @@ else
 end
 
 puts "\nHours from now vs Precipitation probability"
-hourly_hash = parsed_response.fetch("hourly")
-hour_data = hourly_hash.fetch("data")
-next_12_hours = hour_data[0..11]
-# hour_precip = hour_data[0].fetch("precipProbability")
+
 precip_array = next_12_hours.map {|hour| hour.fetch("precipProbability")}
 chart_array = precip_array.each_with_index.map {|data, index| [index+1, data]}
 puts AsciiCharts::Cartesian.new(chart_array, :bar => true, :hide_zero => true).draw
